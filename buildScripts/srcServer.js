a0e4c3f0eedeab349581f6101938364bdc693f4d
Dragos Hodina <dragos.hodina@iquestgroup.com>
2017-03-09 17:06:25 +0200

    Added simple api for getting users

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,13 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  res.json([
+    {"id": 1, "firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
+    {"id": 2, "firstName":"Alex", "lastName":"Tric", "email":"alex@gmail.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if(err) {
     console.log(err);
