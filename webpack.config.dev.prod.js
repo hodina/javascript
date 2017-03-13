e5f132c66e63b0e6c535670d652c63ed30347c9e
Dragos Hodina <dragos.hodina@iquestgroup.com>
2017-03-13 15:00:09 +0200

    Final version

---
+++ webpack.config.dev.prod.js
@@ -0,0 +1,51 @@
+ import path from 'path';
+ import webpack from 'webpack';
+
+export default {
+
+  debug: true,
+
+  devtool: 'source-map',
+
+  noInfo: false,
+
+  entry: [
+
+    path.resolve(__dirname, 'dist')
+
+  ],
+
+  target: 'web',
+
+  output: {
+
+    path: path.resolve(__dirname, 'src'),
+
+    publicPath: '/',
+
+    filename: 'bundle.js'
+
+  },
+
+  plugins: [
+
+  //eliminate duplicate packages when generating bundle
+  new webpack.optimize.DedupePlugin(),
+
+  // minify js
+  new webpack.optimize.UglifyJsPlugin()
+  ],
+
+  module: {
+
+    loaders: [
+
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+
+      {test: /\.css$/, loaders: ['style','css']}
+
+    ]
+
+  }
+
+}
